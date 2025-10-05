6ec2f5046dbe264e38c79c5393c98139c0fe2251
Nguyen Van Thai <nvthai2904@gmail.com>
2025-10-05 20:17:19 +0700

    Custom Dynamic Middleware for ApiKey and Permissions

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const { findByKey } = require("../services/apikey.service");
+
+const HEADER = {
+  API_KEY: "x-api-key",
+  AUTHORIZATION: "authorization",
+};
+
+const apiKey = async (req, res, next) => {
+  try {
+    //check apiKey in headers
+    const apiKey = req.headers[HEADER.API_KEY].toString();
+
+    if (!apiKey) {
+      return res.status(403).json({
+        code: 403,
+        message: "Forbidden",
+      });
+    }
+    //check apiKey in db
+    const objKey = await findByKey(apiKey);
+    if (!objKey) {
+      return res.status(403).json({
+        code: 403,
+        message: "Forbidden",
+      });
+    }
+
+    req.objKey = objKey;
+    return next();
+  } catch (error) {
+    console.error("Error apiKey middleware", error);
+    return res.status(403).json({
+      code: 403,
+      message: "Forbidden",
+    });
+  }
+};
+
+const permission = (permission) => {
+  return (req, res, next) => {
+    try {
+      const permissions = req.objKey.permissions || [];
+      const check = permissions.includes(permission);
+      if (!check) {
+        console.log("Permission denied", permission, permissions);
+        return res.status(403).json({
+          code: 403,
+          message: "permission denied",
+        });
+      }
+      return next();
+    } catch (error) {
+      console.error("Error permission middleware", error);
+      return res.status(403).json({
+        code: 403,
+        message: "Forbidden",
+      });
+    }
+  };
+};
+
+module.exports = {
+  apiKey,
+  permission,
+};
---
+++ src/services/apikey.service.js
@@ -0,0 +1,12 @@
+'use strict";';
+
+const apiKeyModel = require("../models/apikey.model");
+
+const findByKey = async (key) => {
+  const objKey = await apiKeyModel.findOne({ key, status: true }).lean();
+  return objKey;
+};
+
+module.exports = {
+  findByKey,
+};
